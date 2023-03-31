cc8ffa0483959680cb89028461bd8d5d6cd4ae56
Gaston Recalde <gastonrg94@gmail.com>
2023-03-31 18:36:23 -0300

    Archivo app.js con funcion de boton en menu añadido

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const navOpen = document.querySelector('.icon__open');
+const navClose = document.querySelector('.icon__close');
+const nav = document.querySelector('.nav__responsive');
+
+navOpen.addEventListener('click', () => {
+    navOpen.style.display = 'none';
+    navClose.style.display = 'inline-block';
+    nav.classList.add('nav__responsive-active');
+});
+
+navClose.addEventListener('click', () => {
+    navOpen.style.display = 'inline-block';
+    navClose.style.display = 'none';
+    nav.classList.remove('nav__responsive-active');
+});
