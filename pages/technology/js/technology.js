fd99b7529e6a79db90b7fc6e7a83d3c64e412ca3
Gaston Recalde <gastonrg94@gmail.com>
2023-04-06 11:28:05 -0300

    Technology desktop css añadido

--- pages/technology/js/technology.js
+++ pages/technology/js/technology.js
@@ -1,42 +1,5 @@
-// const technology1 = document.getElementById('technology1');
-// const technology2 = document.getElementById('technology2');
-// const technology3 = document.getElementById('technology3');
 
-// technology1.addEventListener('click', () => {
-//   fetch('../../data.json')
-//     .then(response => response.json())
-//     .then(data => {
-//       const technology = data.technology[0];
-//       document.getElementById('name').textContent = technology.name;
-//       document.getElementById('img').src = technology.images.landscape;
-//       document.getElementById('description').textContent = technology.description;
-//     });
-// });
-
-// technology2.addEventListener('click', () => {
-//     fetch('../../data.json')
-//       .then(response => response.json())
-//       .then(data => {
-//         const technology = data.technology[1];
-//         document.getElementById('name').textContent = technology.name;
-//         document.getElementById('img').src = technology.images.landscape;
-//         document.getElementById('description').textContent = technology.description;
-//       });
-//   });
-
-//   technology3.addEventListener('click', () => {
-//     fetch('../../data.json')
-//       .then(response => response.json())
-//       .then(data => {
-//         const technology = data.technology[2];
-//         document.getElementById('name').textContent = technology.name;
-//         document.getElementById('img').src = technology.images.landscape;
-//         document.getElementById('description').textContent = technology.description;
-//       });
-//   });
-
-  // A Probar luego
-  // Cargar el archivo JSON
+// Cargar el archivo JSON
 const url = '../../data.json';
 fetch(url)
   .then(response => response.json())
@@ -46,12 +9,20 @@ fetch(url)
       li.addEventListener('click', () => {
         // Obtener los datos correspondientes del JSON
         const nombre = data.technology[index].name;
-        const imagen = data.technology[index].images.landscape;
+        // const imagen = data.technology[index].images.landscape;
         const descripcion = data.technology[index].description;
+
+        if(window.innerWidth >= 1200){
+          const imagen = data.technology[index].images.portrait;
+          document.querySelector('#img2').setAttribute('src', imagen);
+        } else{
+          const imagen = data.technology[index].images.landscape;
+          document.querySelector('#img').setAttribute('src', imagen);
+        }
 
         // Modificar el HTML
         document.querySelector('#name').textContent = nombre;
-        document.querySelector('#img').setAttribute('src', imagen);
+        // document.querySelector('#img').setAttribute('src', imagen);
         document.querySelector('#description').textContent = descripcion;
       });
     });
